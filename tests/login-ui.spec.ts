c1856066556cda697dde97da67df4a1f0d9c5afe
Sushant Auti <sushantauti@gmail.com>
2026-05-16 15:30:36 +0530

    Added beforeEach Method

--- tests/login-ui.spec.ts
+++ tests/login-ui.spec.ts
@@ -1,19 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
+
 test.describe('OrangeHRM Login', () => {
   test('Verify Title', async ({ page }) => {
-    await page.goto('/');
+
     await expect(page).toHaveTitle('OrangeHRM');
   });
 
-  test('Login with valid credentials', async ({ page }) => {
-  
-  await page.goto('/');
+  test('Login User is on Login page', async ({ page }) => {
   await expect(page.locator("xpath=//h5[text()='Login']")).toHaveText('Login');
  });
 
  test('Login with invalid credentials', async ({ page }) => {
-     await page.goto('/');
      await expect(page.locator("xpath=//input[@name='username']")).toHaveAttribute('placeholder', 'Username');
      await expect(page.locator("xpath=//input[@name='password']")).toHaveAttribute('placeholder', 'Password');
  });

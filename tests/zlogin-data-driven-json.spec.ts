fbdf40612fa05d6442ce97a30fbfb416ca838cf6
Sushant Auti <sushantauti@gmail.com>
2026-05-17 11:56:47 +0530

    updated test cases

--- tests/zlogin-data-driven-json.spec.ts
+++ tests/zlogin-data-driven-json.spec.ts
@@ -4,15 +4,16 @@ import { DashboardPage } from '../pages/Dashboardpage.js';
 import { JsonUtil } from '../utils/json-util.js';
 
 
-
+const validLoginData = JsonUtil.readJsonFile('validLoginData');
+    for (const { username, password, errorMessage } of validLoginData) {
 test.describe('OrangeHRM Login - Data Driven', () => {
     test('Validate user is able to Login', async ({ page }) => {
         const loginPage = new LoginPage(page);
-        await loginPage.fillUsername("Admin");
-        await loginPage.fillPassword("admin123");
+        await loginPage.fillUsername(username);
+        await loginPage.fillPassword(password);
         await loginPage.clickOnLogin();
         const dashboardPage = new DashboardPage(page);
-        await dashboardPage.validateDashboardHeader("Dashboard");
+        await dashboardPage.validateDashboardHeader(errorMessage);
     });
 
     const invalidLoginData = JsonUtil.readJsonFile('invalidLoginData');

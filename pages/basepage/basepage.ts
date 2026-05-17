fe40a66695678b132bdf4bce1d7655e4c040a25d
Sushant Auti <sushantauti@gmail.com>
2026-05-17 12:43:51 +0530

    added basepage.ts

---
+++ pages/basepage/basepage.ts
@@ -0,0 +1,17 @@
+import { expect, type Page } from '../../fixures/base-test.js';
+
+export default class Playwrightkeywords {
+  private readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  public async sendTextToElement(locator: string, text: string): Promise<void> {
+    await this.page.fill(locator, text);
+  }
+
+    public async clickElement(locator: string): Promise<void> {
+    await this.page.click(locator);
+  }
+}
